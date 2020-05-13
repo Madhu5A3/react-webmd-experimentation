abeecbb45d336ce63cb3ec2b1fafd782a5325672
Madhu5A3 <madhuv299@gmail.com>
2020-05-13 14:09:37 +0530

    useHistory Hook

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,15 +1,23 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { useHistory } from 'react-router-dom';
 
 const UseEffect = (props) => {
 
-    const [count,setCount] = useState(0);
+    const [count, setCount] = useState(0);
     useEffect(() => {
         document.title = `You clicked ${count} times`;
     });
+
+    let history = useHistory();
+    const handleClick = () =>{
+        history.push("/");
+    }
     return (
         <div className="useeffect">
             <p>use effect hook: {count}</p>
             <button onClick={() => setCount(count + 1)}>Press</button>
+            <button onClick={handleClick}>Click</button>
         </div>
     );
 }
