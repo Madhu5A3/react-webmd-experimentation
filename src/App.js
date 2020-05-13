5af6e4e2a5182016c7736845d24a6117c218772c
root <root@e0284l.lsz.ads.valuelabs.net>
2020-05-13 14:49:22 +0530

    useLocation Hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import HookComponent from './components/HookComponent';
-import UseEffect from './components/UseEffect';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import UseEffect from './components/UseEffect';
+import { BrowserRouter, Route, Switch, useParams } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import UseLocation from './components/UseLocation';
+// import UseParams from './components/UseParams';
+
+function UseParams() {
+  let {params} = useParams();
+  return (
+    <div className="params experimentation">
+      <p>Displaying: {params}</p>
+    </div>
+  )
+}
 
 function App() {
   return (
@@ -11,7 +22,10 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path="/" component={HookComponent}></Route>
-          <Route path="/useEffect" component={UseEffect}></Route>
+          <Route path="/useEffect/:params">
+            <UseParams />
+          </Route>
+          <Route path="/useLocation" component={UseLocation}></Route>
         </Switch>
       </BrowserRouter>
     </div>
