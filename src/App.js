f722d3c1a4f71ff43a65ea4400b2729c596d50c9
Madhu Kiran Varma <madhuv299@gmail.com>
2020-05-14 21:44:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import HookComponent from './components/HookComponent';
-// import UseEffect from './components/UseEffect';
-import { BrowserRouter, Route, Switch, useParams } from 'react-router-dom';
+import UseEffect from './components/UseEffect';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
